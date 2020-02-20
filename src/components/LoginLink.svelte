5709b9d36682a2229355a2e6e7a3bf5f8d478745
Chris Yi <chris@Chris.local>
2020-02-20 10:48:40 -0500

    first commit

---
+++ src/components/LoginLink.svelte
@@ -0,0 +1,30 @@
+<script>
+  import user from "../stores/user";
+  import { link } from "svelte-routing";
+  import globalStore from "../stores/globalStore";
+
+  //logout function
+</script>
+
+{#if $user.jwt}
+  <!-- content here -->
+  <a
+    href="/"
+    class="logout-btn"
+    use:link
+    on:click={() => {
+      globalStore.toggleItem('sidebar', false);
+    }}>
+    Logout
+  </a>
+{:else}
+  <!-- else content here -->
+  <a
+    href="/login"
+    use:link
+    on:click={() => {
+      globalStore.toggleItem('sidebar', false);
+    }}>
+    login
+  </a>
+{/if}
